54b3a8b1d7cec54256c7e1ef1b6dbe561cc6576e
Ademar <ademarsantiago@gmail.com>
2023-07-08 10:49:44 -0300

    commit inicial projeto ajs_virtual java

---
+++ src/main/java/ajs/dev/virtual/AjsVirtualApplication.java
@@ -0,0 +1,13 @@
+package ajs.dev.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjsVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjsVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajs/dev/virtual/AjsVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package ajs.dev.virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjsVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
